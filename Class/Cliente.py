d000fea4325f5911c960ba69bb9b0e90e4583871
jordi1307 <jluesma93@gmail.com>
2014-06-13 15:33:06 +0200

    Commit de clase Cliente

--- Class/Cliente.py
+++ Class/Cliente.py
@@ -1,1 +1,19 @@
 __author__ = 'jor'
+
+class cliente:
+    nombre=""
+    apellido=""
+    nif=""
+    def __init__(self,nombre,apellido,nif):
+        self.nombre=nombre
+        self.apellido=apellido
+        self.nif=nif
+
+
+    def getNif(self):
+        return self.nif
+    def getNombre(self):
+        return self.nombre
+    def getApellido(self):
+        return self.apellido
+
