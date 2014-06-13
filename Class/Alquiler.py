d89380b2b23a29a42926634cae055bf0569918a4
jordi1307 <jluesma93@gmail.com>
2014-06-13 15:43:30 +0200

    Commit de clase Alquiler

--- Class/Alquiler.py
+++ Class/Alquiler.py
@@ -1,1 +1,39 @@
 __author__ = 'jor'
+
+class alquiler:
+    matricula=""
+    nif=""
+    fecha_alquiler=""
+    fecha_debolucion=""
+    importe=""
+    completada=False
+    def __init__(self,matricula,nif, fecha_alquiler,fecha_debolucion,importe,completada):
+        self.matricula=matricula
+        self.nif=nif
+        self.fecha_alquiler=fecha_alquiler
+        self.fecha_debolucion=fecha_debolucion
+        self.importe=importe
+        self.completada=completada
+
+
+
+    def getMatricula(self):
+        return self.matricula
+
+    def getNif(self):
+        return self.nif
+
+    def getFecha_alquiler(self):
+        return self.fecha_alquiler
+
+    def getFecha_debolucion(self):
+        return self.fecha_debolucion
+
+    def getImporte(self):
+        return self.importe
+
+    def getCompletada(self):
+        return self.completada
+
+
+
