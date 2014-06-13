7938bdde997eabdbe85bb8253e4e36b9f9647dd3
jordi1307 <jluesma93@gmail.com>
2014-06-13 15:30:14 +0200

    Commit de clase Coche

--- Class/Coche.py
+++ Class/Coche.py
@@ -1,1 +1,33 @@
 __author__ = 'jor'
+class coche:
+    matricula=""
+    marca=""
+    modelo=""
+    precio_dia=""
+    disponible = False
+
+    def __init__(self,matricula,marca,modelo,precio_dia,disponible):
+            self.matricula=matricula
+            self.marca=marca
+            self.modelo=modelo
+            self.precio_dia=precio_dia
+            self.disponible = disponible
+
+    def getMatricula(self):
+        return self.matricula
+    def getMarca(self):
+        return self.marca
+    def getModelo(self):
+        return self.modelo
+    def getPrecio_dia(self):
+        return self.precio_dia
+    def getDisponible(self):
+        return self.disponible
+
+
+
+    def setDisponible(self,disponible):
+        self.disponible=disponible
+    def setPrecio_dia(self,precio_dia):
+        self.precio_dia=precio_dia
+
